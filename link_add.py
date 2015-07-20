84ad5f056e9c2fd3669e2e6e0e22553e055973d1
Gennadii <g.mishchevskii@gmail.com>
2015-07-20 13:38:32 +0300

    Added wanted list

--- link_add.py
+++ link_add.py
@@ -3,42 +3,57 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementNotVisibleException
 from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
+import os.path
+
+my_login = 'g.mishchevskii@gmail.com'
+my_password = input('Password: ')
+wanted_list = ['hr', 'recruiter', 'рекрутер', 'recruitment', 'open', 'looking', 'talent','resercher', 'wanted']
 
 count = 0
 people_number = int(input('Enter a number of people you want to add: '))
 
-driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\chromedriver.exe')
+driver = webdriver.Chrome(os.path.expanduser(r'~\Dropbox\Work\Python\chromedriver.exe'))
 driver.implicitly_wait(60)
 driver.get('https://www.linkedin.com/nhome/')
-driver.find_element_by_xpath("//div[@class='fieldgroup']/input[@id='session_key-login']").send_keys('*')
-driver.find_element_by_xpath("//div[@class='fieldgroup']/input[@id='session_password-login']").send_keys('*')
-driver.find_element_by_xpath("//li[@class='button']/input[@id='signin']").click()
+
+log_in = driver.find_element_by_xpath("//div[@class='fieldgroup']/input[@id='session_key-login']")
+password = driver.find_element_by_xpath("//div[@class='fieldgroup']/input[@id='session_password-login']")
+sign_in_button = driver.find_element_by_xpath("//li[@class='button']/input[@id='signin']")
+
+log_in.send_keys(my_login)
+password.send_keys(my_password)
+sign_in_button.click()
 
 while count < people_number:
     driver.get('https://www.linkedin.com/people/pymk/hub?trk=hp-feed-xconny-icon')
     driver.implicitly_wait(10)
+
     people = driver.find_elements_by_xpath("//div[@class='card-wrapper']//p[@class='headline']/span[@title]")
     buttons = driver.find_elements_by_xpath("//div[@class='card-wrapper']//button[@data-act='request']")
     card = driver.find_elements_by_xpath("//div[@class='card-wrapper']")
     delete = driver.find_elements_by_xpath("//button[@class='bt-close']")
+
     num = 0
-    # sleep(3)
     for person in card:
-        if 'hr' in people[num].text.lower() or 'recruiter' in people[num].text.lower() or 'рекрутер' in people[num].text.lower() \
-        or 'recruitment' in people[num].text.lower() or 'open' in people[num].text.lower() or 'looking' in people[num].text.lower() or \
-        'talent' in people[num].text.lower() or 'resercher' in people[num].text.lower() or 'wanted' in people[num].text.lower():
-            buttons[num].click()
-            count += 1
-            print(str(count))
-        else:
+        flag = False
+        for wanted in wanted_list:
+            if wanted in people[num].text.lower():
+                buttons[num].click()
+                count += 1
+                print(str(count) + ' - ')
+                break
+            else:
+                flag = True
+
+        if flag:
             ActionChains(driver).move_to_element(person).perform()
             try:
                 delete[num].click()
             except ElementNotVisibleException:
                 pass
+        
         num += 1
-        if num == 8:
+        if num > 7:
             break
 
 driver.quit()
