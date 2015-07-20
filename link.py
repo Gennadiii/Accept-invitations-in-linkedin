55977bb642c6c5efd1b88bee18a69ec1084a2ae3
Gennadii <g.mishchevskii@gmail.com>
2015-07-20 16:59:51 +0300

    Redesign

--- link.py
+++ link.py
@@ -1,16 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+import os.path
 
+my_login = 'g.mishchevskii@gmail.com'
+my_password = input('Password: ')
 count = 0
 
 driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\chromedriver.exe')
-driver.implicitly_wait(60)
 driver.get('https://www.linkedin.com/nhome/')
-driver.find_element_by_xpath("//div[@class='fieldgroup']/input[@id='session_key-login']").send_keys('*')
-driver.find_element_by_xpath("//div[@class='fieldgroup']/input[@id='session_password-login']").send_keys('*')
-driver.find_element_by_xpath("//li[@class='button']/input[@id='signin']").click()
+driver.implicitly_wait(60)
+
+log_in = driver.find_element_by_xpath("//div[@class='fieldgroup']/input[@id='session_key-login']")
+password = driver.find_element_by_xpath("//div[@class='fieldgroup']/input[@id='session_password-login']")
+sign_in_button = driver.find_element_by_xpath("//li[@class='button']/input[@id='signin']")
+
+log_in.send_keys(my_login)
+password.send_keys(my_password)
+sign_in_button.click()
+
 driver.get('https://www.linkedin.com/inbox/#invitations')
+driver.implicitly_wait(60)
+
+names = driver.find_elements_by_xpath("//p[@class='participants']//a")
+positions = driver.find_elements_by_xpath("//p[@class='headline']")
+
 try: 
 	invitations = driver.find_elements_by_xpath("//a[@class='accept accept-invite']")
 except NoSuchElementException:
@@ -18,15 +32,15 @@ except NoSuchElementException:
 	print('Nothing to accept')
 	exit()
 
-names = driver.find_elements_by_xpath("//p[@class='participants']//a")
-positions = driver.find_elements_by_xpath("//p[@class='headline']")
-
 num = 0
 for name in names:
 	try:
 		print('\n' + '{:*>80}'.format(name.text) + '\n' + '{:>80}'.format(positions[num].text) + '\n' + name.get_attribute('href'))
 	except UnicodeEncodeError:
-		print('\n' + '{:*>80}'.format(name.text) + '\n' + name.get_attribute('href'))
+		try:
+			print('\n' + '{:*>80}'.format(name.text) + '\n' + name.get_attribute('href'))
+		except UnicodeEncodeError:
+			print('\n' + name.get_attribute('href'))
 	num += 1
 
 action = input('\nStart accepting? ')
@@ -35,7 +49,6 @@ if action == 'no':
 	exit()
 
 for invitation in invitations:
-	driver.implicitly_wait(5)
 	invitation.click()
 	count += 1
 print(str(count) + ' invitations are accepted')
