654c2313e4c766a648f45b5628ce5f1092d4de8d
Gennadii <g.mishchevskii@gmail.com>
2015-09-15 09:52:02 +0300

    Create link_cut_v_2.py

---
+++ link_cut_v_2.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from os.path import expanduser
+from time import sleep
+
+count = 0
+
+driver = webdriver.Chrome(expanduser(r'~\Dropbox\Work\Python\chromedriver.exe'))
+driver.get('https://www.linkedin.com/nhome/')
+driver.implicitly_wait(60)
+
+input('Let\'s roll!')
+
+while True:
+	driver.get('https://www.linkedin.com/inbox/#invitations')
+	driver.implicitly_wait(60)
+	if driver.find_element_by_xpath("//a[@href='/inbox/#invitations']").text == 'Invitations':
+		break
+	invitations = driver.find_elements_by_xpath("//a[@class='accept accept-invite']")
+	for invitation in invitations:
+		invitation.click()
+		sleep(3)
+		count += 1
+
+driver.quit()
+input(str(count) + ' invitations have been accepted.')
+exit()
